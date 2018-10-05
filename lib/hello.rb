c35348a238f0930625e9981be044ceab4f0fb5c9
Yu Li <yli0607x@gmail.com>
2018-10-05 14:14:55 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -10,11 +10,3 @@ else
   puts "Hey! No block was given!"
 end	end
 end
-
-
-# call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
